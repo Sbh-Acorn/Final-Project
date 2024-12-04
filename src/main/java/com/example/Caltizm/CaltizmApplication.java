06bc62d48f162fd4ee3016fa6f59245fc46439c5
sbh0609 <49151146+sbh0609@users.noreply.github.com>
2024-12-04 16:55:57 +0900

    Initial commit

---
+++ src/main/java/com/example/Caltizm/CaltizmApplication.java
@@ -0,0 +1,13 @@
+package com.example.Caltizm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltizmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaltizmApplication.class, args);
+	}
+
+}
