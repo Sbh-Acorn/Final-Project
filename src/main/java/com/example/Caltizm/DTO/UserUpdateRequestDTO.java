d116f0fa8b81a1f2940d00f2ccc41e1cf5988893
blueming562 <blueming562@gmail.com>
2024-12-07 17:14:57 +0900

    feat: My Page

--- src/main/java/com/example/Caltizm/DTO/UserUpdateRequestDTO.java
+++ src/main/java/com/example/Caltizm/DTO/UserUpdateRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class UserUpdateRequestDTO {
 
-    private String userId;
+    private String email;
     private String firstName;
     private String lastName;
     private String phoneNumber;
