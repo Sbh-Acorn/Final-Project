e9ccc2609244ef78769aa62c2c1fbae58c29e32b
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2024-12-16 23:02:21 +0900

    front add

---
+++ src/main/resources/static/js/mypage.js
@@ -0,0 +1,106 @@
+const $tabs = document.querySelectorAll(".tab");
+const tabLength = $tabs.length;
+
+let $info = document.querySelector("#info_box");
+let $pwd = document.querySelector("#passwd_box");
+let $board = document.querySelector("#board_box");
+
+let $update = document.querySelector("#update_btn");
+let $address = document.querySelector("#address_btn");
+
+const $tabWrap = document.getElementById("tab_wrap");
+const $addressInfoWrap = document.getElementById("address_info_wrap");
+
+$update.addEventListener("click", () => {
+
+    const $infoInput = document.querySelectorAll(".info_input");
+    const btnText = $update.textContent.trim();
+
+    if (btnText === '정보일괄수정') {
+
+        $tabWrap.style.pointerEvents = 'none';
+        $addressInfoWrap.style.pointerEvents = 'none';
+
+
+        $infoInput.forEach(element => {
+
+            element.addEventListener('focus', () => {
+                element.style.outline = '1px solid black';
+            });
+            element.addEventListener('blur', () => {
+                element.style.outline = 'none';
+            });
+
+            element.removeAttribute("readonly");
+            element.style.outline = 'none';
+            $update.innerText = "정보수정완료";
+        });
+        $infoInput[0].focus();
+    } else {
+        if (checkNum()) {
+            $tabWrap.style.pointerEvents = 'auto';
+            $addressInfoWrap.style.pointerEvents = 'auto';
+
+            $infoInput.forEach(element => {
+
+                element.addEventListener('focus', () => {
+                    element.style.outline = '0';
+                });
+
+                element.setAttribute("readonly", true);
+                element.style.outline = '0';
+                $update.innerText = "정보일괄수정";
+            });
+        }
+    }
+});
+
+$address.addEventListener("click", () => {
+    document.querySelector("#modal1").style.display = 'flex';
+});
+
+for (let i = 0; i < tabLength; i++) {
+    $tabs[i].addEventListener("click", () => {
+        $tabs[i].style.borderTop = '1px solid rgba(148, 147, 147, 0.4)';
+        $tabs[i].style.borderLeft = '1px solid rgba(148, 147, 147, 0.4)';
+        $tabs[i].style.borderRight = '1px solid rgba(148, 147, 147, 0.4)';
+        $tabs[i].style.borderBottom = '1px solid white';
+        for (let j = 0; j < tabLength; j++) {
+            if (j === i) continue;
+            else $tabs[j].style.border = '0px';
+        }
+
+        switch(i) {
+            case 0:
+                $info.style.display = 'block';
+                $pwd.style.display = 'none';
+                $board.style.display = 'none';
+                break;
+            case 1:
+                $info.style.display = 'none';
+                $pwd.style.display = 'block';
+                $board.style.display = 'none';
+                break;
+            case 2:
+                $info.style.display = 'none';
+                $pwd.style.display = 'none';
+                $board.style.display = 'block';
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+function checkNum() {
+
+    const $infoInput = document.querySelectorAll(".info_input");
+
+    for (let i = 0; i < 4; i++) {
+        if ($infoInput[i].value.trim() === '') {
+            $infoInput[i].focus();
+            return false;
+        };
+    }
+    return true;
+}
