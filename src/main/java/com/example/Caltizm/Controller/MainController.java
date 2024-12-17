cb20152caf87efeb01a9b414c8531fc9ae790628
sbh0609 <49151146+sbh0609@users.noreply.github.com>
2024-12-18 03:51:53 +0900

    feat: get banner images use session

--- src/main/java/com/example/Caltizm/Controller/MainController.java
+++ src/main/java/com/example/Caltizm/Controller/MainController.java
@@ -1,10 +1,9 @@
 package com.example.Caltizm.Controller;
 
-
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
@@ -12,14 +11,20 @@ import java.util.List;
 public class MainController {
 
     @GetMapping("/main")
-    public String mainPage(Model model){
+    public String mainPage(Model model, HttpSession session) {
+        // 배너 이미지 리스트 추가
         List<String> bannerImages = List.of(
                 "/bannerImages/Coupon-35-im.jpg",
                 "/bannerImages/blackbuy.webp",
                 "/bannerImages/3.webp",
                 "/bannerImages/4.webp"
         );
-        model.addAttribute("BannerImages",bannerImages);
+        model.addAttribute("BannerImages", bannerImages);
+
+        // 세션에서 email 값 가져오기
+        String email = (String) session.getAttribute("email");
+        model.addAttribute("sessionEmail", email);
+
         return "main/main";
     }
 }
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -4,13 +4,16 @@ let $bucket = document.querySelector("#header_bucket");
 
 let $alarm_drop = document.querySelector("#header_alarm_dropdown");
 let $profile_drop = document.querySelector("#header_profile_dropdown");
+let $drop = document.querySelector(".dropdown")
+
 
 $alarm.addEventListener("click", () => {
-    $profile_drop.classList.remove("active");
+    $drop.classList.remove("active");
     $alarm_drop.classList.toggle("active");
 });
 
 $profile.addEventListener("click", () => {
     $alarm_drop.classList.remove("active");
-    $profile_drop.classList.toggle("active");
+//    $profile_drop.classList.toggle("active");
+    $drop.classList.toggle("active");
 });
