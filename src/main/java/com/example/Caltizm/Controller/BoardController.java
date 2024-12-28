a0e519fb8c1c0a13a3921619a2dffdeb2e24029a
blueming562 <blueming562@gmail.com>
2024-12-28 15:53:11 +0900

    feat: Board

--- src/main/java/com/example/Caltizm/Controller/BoardController.java
+++ src/main/java/com/example/Caltizm/Controller/BoardController.java
@@ -17,6 +17,8 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import java.util.List;
@@ -27,20 +29,6 @@ public class BoardController {
     @Autowired
     BoardRepository repository;
 
-    @GetMapping("/board_ui")
-    public String boardUi(Model model, HttpSession session) {
-
-        String email = (String) session.getAttribute("email");
-        model.addAttribute("sessionEmail", email);
-      
-        return "board/board_main_ui";
-    }
-
-    @GetMapping("/testBoard")
-    public String tb() {
-        return "board/board_main_ui";
-    }
-
     // 전체 게시판 조회
     @GetMapping("/boardAll")
     public String boardAll(Model model){
@@ -50,8 +38,9 @@ public class BoardController {
         model.addAttribute("hotview", hotview);
         model.addAttribute("boardNotice", boardNotice);
         model.addAttribute("boardList",boardList);
+        model.addAttribute("boardName", "all");
 
-        return "board/board_main_ui";
+        return "board/board_main";
     }
 
     // 공지사항 조회
@@ -63,7 +52,8 @@ public class BoardController {
         model.addAttribute("hotview", hotview);
         model.addAttribute("boardNotice", boardNotice);
         model.addAttribute("boardList",boardList);
-        return "board/board_main_ui";
+        model.addAttribute("boardName", "notice");
+        return "board/board_main";
     }
 
     // 자유게시판 조회
@@ -75,7 +65,8 @@ public class BoardController {
         model.addAttribute("hotview", hotview);
         model.addAttribute("boardNotice", boardNotice);
         model.addAttribute("boardList", boardList);
-        return "board/board_main_ui";
+        model.addAttribute("boardName", "free");
+        return "board/board_main";
     }
 
     // 리뷰 조회
@@ -87,7 +78,8 @@ public class BoardController {
         model.addAttribute("hotview", hotview);
         model.addAttribute("boardNotice", boardNotice);
         model.addAttribute("boardList", boardList);
-        return "board/board_main_ui";
+        model.addAttribute("boardName", "review");
+        return "board/board_main";
     }
 
     // Q&A 조회
@@ -99,29 +91,18 @@ public class BoardController {
         model.addAttribute("hotview", hotview);
         model.addAttribute("boardNotice", boardNotice);
         model.addAttribute("boardList",boardList);
-        return "board/board_main_ui";
+        model.addAttribute("boardName", "qna");
+        return "board/board_main";
     }
 
     //Test
     @GetMapping("/post")
     public String postTest(Model model){
 
-        List<PostDTO> boardList = repository.selectAll();
-        model.addAttribute("boardAll",boardList);
+        List<PostDTO> hotview = repository.hotview();
+        model.addAttribute("hotview", hotview);
 
-        List<PostDTO> boardList2 = repository.selectNotice();
-        model.addAttribute("boardNotice",boardList2);
-
-        List<PostDTO> boardList3 = repository.selectFree();
-        model.addAttribute("boardFree", boardList3);
-
-        List<PostDTO> boardList4 = repository.selectReview();
-        model.addAttribute("boardReview", boardList4);
-
-        List<PostDTO> boardList5 = repository.selectQna();
-        model.addAttribute("boardQna",boardList5);
-
-        return "board/formtest";
+        return "board/board_write";
     }
 
     // 게시판 작성
@@ -202,12 +183,23 @@ public class BoardController {
         List<PostDTO> hotview = repository.hotview();
         model.addAttribute("hotview", hotview);
 
+        model.addAttribute("boardName", post.getSubject());
+
         if(email != null) {
             int user =  repository.getUser(email);
             model.addAttribute("user",user);
+
+            // 사용자의 게시글 추천 여부 확인
+            int user_id = repository.getUser(email);
+            int count = repository.checkLikes(post_id, user_id);
+            if(count > 0){
+                model.addAttribute("isLiked", true);
+            } else{
+                model.addAttribute("isLiked", false);
+            }
         }
 
-        return "board/board_ui";
+        return "board/board";
     }
 
     // 게시글 삭제
@@ -223,7 +215,9 @@ public class BoardController {
                            Model model){
         PostDTO post = repository.selectPostOne(post_id);
         model.addAttribute("postOne", post);
-        return "board/editpost";
+        List<PostDTO> hotview = repository.hotview();
+        model.addAttribute("hotview", hotview);
+        return "board/board_edit";
     }
     
     // 게시글 수정
@@ -241,11 +235,13 @@ public class BoardController {
     // 게시글 추천
     @ResponseBody
     @PostMapping("/likePost")
-    public int likePost(@RequestParam(value="post_id") String post_id,
-                        @SessionAttribute(value = "email") String email) {
+    public Map<String, Object> likePost(@RequestParam(value="post_id") String post_id,
+                                        @SessionAttribute(value = "email") String email) {
 
         System.out.println(post_id);
         System.out.println(email);
+
+        Map<String, Object> response = new HashMap<>();
 
         int user_id = repository.getUser(email);
         int count = repository.checkLikes(Integer.parseInt(post_id), user_id);
@@ -253,11 +249,17 @@ public class BoardController {
         if(count == 0){
             repository.incLikes(Integer.parseInt(post_id));
             repository.insertLikes(Integer.parseInt(post_id), user_id);
+            response.put("status", "likes_increased");
         } else {
             repository.decLikes(Integer.parseInt(post_id));
             repository.deleteLikes(Integer.parseInt(post_id), user_id);
+            response.put("status", "likes_decreased");
         }
-        return repository.countLikes(Integer.parseInt(post_id));
+
+        response.put("likes", repository.countLikes(Integer.parseInt(post_id)));
+        System.out.println(response);
+
+        return response;
     }
 
     // 댓글 작성
