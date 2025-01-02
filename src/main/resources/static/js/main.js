de56872c50bb891d3c657ab44d78f8887ff2df20
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2025-01-02 17:07:02 +0900

    commit
    commit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,25 +2,17 @@ let $banner = document.getElementById('banner');
 let $prevBtn = document.getElementById('prev_btn_wrap');
 let $nextBtn = document.getElementById('next_btn_wrap');
 let $banners = document.querySelectorAll('.banner_img_wrap');
-let $randomText = document.getElementById('random_text');
 
 let bannerCount = $banners.length;
 
 let currentIndex = 0;
 
-const texts = [
-    "Welcome to the First Slide",
-    "Explore the Second Slide",
-    "Here Comes the Third Slide",
-    "Enjoy the Fourth Slide"
-];
 
 function moveBanner(index) {
     const offset = -index * 25;
     $banner.style.transform = `translateX(${offset}%)`;
     $banner.style.transition = 'transform 0.5s ease';
 
-    $randomText.textContent = texts[index % bannerCount];
 }
 
 // 자동 이동 기능
