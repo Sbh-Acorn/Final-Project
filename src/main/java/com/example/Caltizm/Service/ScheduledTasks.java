8ee3822ca32d9db87c9eac1d86e9481e21bd3d76
sbh0609 <49151146+sbh0609@users.noreply.github.com>
2025-01-02 15:16:32 +0900

    feat: 초기 데이터 로딩 주석

--- src/main/java/com/example/Caltizm/Service/ScheduledTasks.java
+++ src/main/java/com/example/Caltizm/Service/ScheduledTasks.java
@@ -27,20 +27,20 @@ public class ScheduledTasks {
         try {
 
             //사진 데이터 초기화
-            List<String> bannerImages = getDataService.collectBannerImage();
-            String bannerDirectory = "src/main/resources/static/bannerImages";
-            getDataService.saveImage(bannerImages, bannerDirectory);
-            // 상품 데이터 초기화
-            dataRepository.collectAndInsertData();
-            System.out.println("초기 상품 데이터 삽입 완료");
-
-            // 환율 데이터 초기화
-            Map<String, Double> exchangeRates = exchangeRateService.getExchangeRates();
-
-
-            System.out.println("환율 데이터 초기화 완료");
-            System.out.println("EUR to KRW: " + exchangeRates.get("EUR_TO_KRW"));
-            System.out.println("USD to EUR: " + exchangeRates.get("USD_TO_EUR"));
+//            List<String> bannerImages = getDataService.collectBannerImage();
+//            String bannerDirectory = "src/main/resources/static/bannerImages";
+//            getDataService.saveImage(bannerImages, bannerDirectory);
+//            // 상품 데이터 초기화
+//            dataRepository.collectAndInsertData();
+//            System.out.println("초기 상품 데이터 삽입 완료");
+//
+//            // 환율 데이터 초기화
+//            Map<String, Double> exchangeRates = exchangeRateService.getExchangeRates();
+//
+//
+//            System.out.println("환율 데이터 초기화 완료");
+//            System.out.println("EUR to KRW: " + exchangeRates.get("EUR_TO_KRW"));
+//            System.out.println("USD to EUR: " + exchangeRates.get("USD_TO_EUR"));
 
         } catch (Exception e) {
             System.err.println("초기 데이터 삽입 중 오류 발생:");
@@ -48,11 +48,11 @@ public class ScheduledTasks {
         }
     }
 
-    // 매일 자정(00:00)에 상품 데이터 갱신
-    @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Seoul") // 매일 자정 (한국 시간 기준)
+    // 매일 (03:00)에 상품 데이터 갱신
+    @Scheduled(cron = "0 0 3 * * ?", zone = "Asia/Seoul") // 매일 자정 (한국 시간 기준)
     public void updateProductData() {
         try {
-            dataRepository.collectAndInsertData();
+                dataRepository.collectAndInsertData();
             System.out.println("상품 데이터 갱신 완료");
         } catch (Exception e) {
             System.err.println("상품 데이터 갱신 중 오류 발생:");
@@ -60,8 +60,8 @@ public class ScheduledTasks {
         }
     }
 
-    // 매일 자정(00:00)에 환율 데이터 갱신
-    @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Seoul") // 매일 자정 (한국 시간 기준)
+    // 매일 (03:00)에 환율 데이터 갱신
+    @Scheduled(cron = "0 0 3 * * ?", zone = "Asia/Seoul") // 매일 자정 (한국 시간 기준)
     public void updateExchangeRateData() {
         try {
             Map<String, Double> exchangeRates = exchangeRateService.getExchangeRates();
@@ -70,6 +70,20 @@ public class ScheduledTasks {
             System.out.println("KRW to USD: " + exchangeRates.get("KRW_TO_USD"));
         } catch (Exception e) {
             System.err.println("환율 데이터 갱신 중 오류 발생:");
+            e.printStackTrace();
+        }
+    }
+
+    // 매일 (03:00)에 환율 데이터 갱신
+    @Scheduled(cron = "0 0 3 * * ?", zone = "Asia/Seoul") // 매일 자정 (한국 시간 기준)
+    public void updateBannerImageData() {
+        try {
+            List<String> bannerImages = getDataService.collectBannerImage();
+            String bannerDirectory = "src/main/resources/static/bannerImages";
+            getDataService.saveImage(bannerImages, bannerDirectory);
+            System.out.println("BannerImage 로딩 완료");
+        } catch (Exception e) {
+            System.err.println("BannerImage 로딩 중 오류 발생:");
             e.printStackTrace();
         }
     }
